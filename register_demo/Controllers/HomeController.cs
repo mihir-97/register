263a2bd29d0344be1da9b4ce7d677537b94f14ac
mihir-97 <mihirpatel19.mp@gmail.com>
2020-02-04 10:34:23 +0530

    register_demo added

---
+++ register_demo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace register_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Register(string firstname, string lastname, string address, string gender, string city, int pincode)
+        {
+            return "You Are Registered With - " + firstname + ", " + lastname + "," + address + "," + gender + "," + city + "," + pincode;
+        }
+    }
+}
---
+++ register_demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="register_demo.MvcApplication" Language="C#" %>
